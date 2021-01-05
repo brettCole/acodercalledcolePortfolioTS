dc30716fbe5fa20928635f022169208a4f0beb68
brett cole <acodercalledcole@gmail.com>
2021-01-05 16:33:40 -0500

    converted _app and index to typescript file types

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Homepage() {
+  return (
+    <h1>
+      {`testing`}
+    </h1>
+  )
+}
